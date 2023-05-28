16618772fddbfc969962b149e29a31c131569bcf
Roberto Scardia <roberto.scardia@gmail.com>
2023-05-29 00:04:02 +0200

    completed plots

--- MZsimulation.m
+++ MZsimulation.m
@@ -36,60 +36,153 @@ wavelenght = 1550*nm;
 E_i_laser = 5*V;
 
 alpha = (0: 0.01: 3);
-ER1 = zeros(1, numel(alpha));
-ER2 = zeros(1, numel(alpha));
+ER_kL1 = zeros(numel(kL_factor), numel(alpha));
+ER_kL2 = zeros(numel(kL_factor), numel(alpha));
 
 
-
+mycolors = [1 0 0; 0 1 0; 0 0 1; 1 0 1];
+ax = gca;
+ax.ColorOrder = mycolors;
+figure(1);
 for j = 1:numel(kL_factor)
 
-    Tc_matrix = [cos(kL_factor(i)), -1i*sin(kL_factor(i)); 
-                 -1i*sin(kL_factor(i)), cos(kL_factor(i))];
+    Tc_matrix = [cos(kL_factor(j)), -1i*sin(kL_factor(j)); 
+                 -1i*sin(kL_factor(j)), cos(kL_factor(j))];
 
-    for i = 1:numel(alpha)
+    for k = 1:numel(alpha)
+
+        % RF HIGH
+        RF_max_input = (wavelenght*gap)/((ne^3)*confinment_factor*r33*Lenght);
+
+        dn=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
+        d_phi = dn*(Lenght).*(2*pi/wavelenght);
+
+        MC_matrix_1 = [exp(-1i*d_phi+(alpha(k)/8.6860000037)), 0; 0, 1];
+        result_vector_low = Tc_matrix*MC_matrix_1*Tc_matrix*[E_i_laser, 0]';
+
+        %RF LOW
+
+        RF_max_input = 0;
+        dn=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
+        d_phi = dn*(Lenght)*(2*pi/wavelenght);
+
+        MC_matrix_2 = [1, 0; 0,1];
+        result_vector_high = Tc_matrix*MC_matrix_2*Tc_matrix*[E_i_laser, 0]';
+    
+        ER_kL1(j, k) = 10*log(abs(result_vector_high(1)^2)/abs(result_vector_low(1)^2));
+        ER_kL2(j, k) = 10*log(abs(result_vector_high(2)^2)/abs(result_vector_low(2)^2));
+    end
+end
+    tiledlayout(1,2)
+
+    %plot ER1
+    nexttile
+    plot(alpha, ER_kL1(1,:), DisplayName="kL Factor = pi/4")
+    hold on
+    plot(alpha, ER_kL1(2,:), DisplayName="kL Factor = pi/3")
+    plot(alpha, ER_kL1(3,:), DisplayName="kL Factor = pi/2")
+    plot(alpha, ER_kL1(4,:), DisplayName="kL Factor = pi/1")
+    hold off
+    legend
+    grid on
+    xlabel("loss [db/m]");
+    ylabel("Extinction rate [db] port 1")
+    title("Extinction rate port 1")
+   
+
+    %plot ER2
+    nexttile
+    plot(alpha, ER_kL2(1,:), DisplayName="kL Factor = pi/4")
+    hold on
+    plot(alpha, ER_kL2(2,:), DisplayName="kL Factor = pi/3")
+    plot(alpha, ER_kL2(3,:), DisplayName="kL Factor = pi/2")
+    plot(alpha, ER_kL2(4,:), DisplayName="kL Factor = pi/1")
+    hold off
+    legend
+    xlabel("loss [db/m]");
+    ylabel("Extinction rate [db] port 2")
+    title("Extinction rate port 2")
+
+
+
+alpha = [0; 0.01 ; 0.05; 0.10; 0.15];
+kL_factor = (-pi/4: 0.01: pi/4);
+
+ER_a1 = zeros(numel(alpha), numel(kL_factor));
+ER_a2 = zeros(numel(alpha), numel(kL_factor));
+
+
+for j = 1:numel(alpha)
+
+    loss = alpha(j)/8.6860000037;
+    for k = 1:numel(kL_factor)
+
+       Tc_matrix = [cos(kL_factor(j)), -1i*sin(kL_factor(j)); 
+                   -1i*sin(kL_factor(j)), cos(kL_factor(j))];
 
         % RF HIGH
         RF_max_input = 5;%(wavelenght*gap)/((ne^3)*confinment_factor*r33*Lenght);
 
-        n=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
-        d_phi = n*(Lenght).*(2*pi/wavelenght);
+        dn=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
+        d_phi = dn*(Lenght).*(2*pi/wavelenght);
 
-        MC_matrix = [exp(-1i*d_phi+(alpha(i)/8.6860000037)), 0; 0, 1];
+        MC_matrix = [exp(-1i*d_phi+loss), 0; 0, 1];
         result_vector_low = Tc_matrix*MC_matrix*Tc_matrix*[E_i_laser, 0]';
 
         %RF LOW
 
         RF_max_input = 0;
-        n=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
-        d_phi = n*(Lenght)*(2*pi/wavelenght);
+        dn=-(ne^3)*r33*confinment_factor*RF_max_input/(2*gap);
+        d_phi = dn*(Lenght)*(2*pi/wavelenght);
 
         MC_matrix = [1, 0; 0,1];
         result_vector_high = Tc_matrix*MC_matrix*Tc_matrix*[E_i_laser, 0]';
     
-        ER1(i) = 20*log(abs(result_vector_high(1))/abs(result_vector_low(1)));
-        ER2(i) = 20*log(abs(result_vector_high(2))/abs(result_vector_low(2)));
-    end 
-    figure(1)
-    tiledlayout(1,2)
+        ER_a1(j, k) = 10*log(abs(result_vector_high(1)^2)/abs(result_vector_low(1)^2));
+        ER_a2(j, k) = 10*log(abs(result_vector_high(2)^2)/abs(result_vector_low(2)^2));
+    end
+end
+
+figure(2)
+tiledlayout(1,2)
 
     %plot ER1
     nexttile
-    plot(alpha, ER1)
-    xlabel("loss [db/m]");
-    ylabel("Extinction rate [db] port 1")
-    title("Ectinction rate port 1")
+    plot(kL_factor, ER_a1(1,:), DisplayName="alpha = 0")
     hold on
+    plot(kL_factor, ER_a1(2,:), DisplayName="alpha = 0.01")
+    plot(kL_factor, ER_a1(3,:), DisplayName="alpha = 0.05")
+    plot(kL_factor, ER_a1(4,:), DisplayName="alpha = 0.10")
+    plot(kL_factor, ER_a1(5,:), DisplayName="alpha = 0.15")
+    hold off
+    legend
+    grid on
+    xlabel("kL factor");
+    ylabel("Extinction rate [db] port 1")
+    title("Extinction rate port 1")
+   
 
     %plot ER2
     nexttile
-    plot(alpha, ER2, 'red')
+    plot(kL_factor, ER_a2(1,:), DisplayName="alpha = 0")
     hold on
-    plot(alpha, 10, '.')
-    xlabel("loss [db/m]");
+    plot(kL_factor, ER_a2(2,:), DisplayName="alpha = 0.01")
+    plot(kL_factor, ER_a2(3,:), DisplayName="alpha = 0.05")
+    plot(kL_factor, ER_a2(4,:), DisplayName="alpha = 0.10")
+    plot(kL_factor, ER_a2(5,:), DisplayName="alpha = 0.15")
+    hold off
+    legend
+    xlabel("kL Factor");
     ylabel("Extinction rate [db] port 2")
-    title("Ectinction rate port 2")
-    hold on
-    
-end
+    title("Extinction rate port 2")
+
+
+
+
+
+
+
+
+
 
 
