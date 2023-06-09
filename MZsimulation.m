e4991fe5b5836483003bf12410b228998dd7d546
Roberto Scardia <roberto.scardia@gmail.com>
2023-06-09 17:16:37 +0200

    fixed ER plots, corrected alpha values, added boxes to plots

--- MZsimulation.m
+++ MZsimulation.m
@@ -10,6 +10,7 @@ um = 1e-6;
 nm = 1e-9;
 pm = 1e-12;
 V = 1;
+dB = 1;
 
 %% GUIDE PARAMETERS
 gap = 6* um;      % gap between electrode
@@ -43,10 +44,10 @@ RF_pi = (wavelength*gap)/((ne^3)*confinment_factor*r33*Length);
 same_loss_second_line  = 0;
 
 
-% Fixed attenuation constant for the second line in [dB/m]. 
+% Fixed attenuation constant for the second line in [dB/cm]. 
 % Irrelevant if 'a_same_loss_second_line' is other than -1.
-% Default = 0*dB
-fixed_loss_second_line = 0;
+% Default = 0 [dB/cm]
+fixed_loss_second_line = 0*(dB/cm);
 
 % Coupling coefficient splitter
 kL_factor_s = [pi/4, (pi/4)*1.10, (pi/4)*0.90, (pi/4)*1.5];
@@ -59,7 +60,7 @@ assert(numel(kL_factor_c) >= numel(kL_factor_s), "kL_factor c has less element t
 %% EXTINTION RATE SIMULATION : fixed coupling coefficient, varying alpha 
 E_i_laser = 1*V;
 
-alpha = (0: 0.01: 3); %[dB/m]
+alpha = (0: 0.01*(dB/cm): 2*(dB/cm)); %[dB/cm]
 ER_kL1 = zeros(numel(kL_factor_s), numel(alpha));
 ER_kL2 = zeros(numel(kL_factor_s), numel(alpha));
 
@@ -102,7 +103,7 @@ for j = 1:numel(kL_factor_s)
         
         r1 = Tc_matrix_c*MC_matrix_1*Tc_matrix_s*[E_i_laser, 0]';
         
-        result_vector_low(1,k,j) = r1(1);
+        result_vector_high(1,k,j) = r1(1);
         result_vector_low(2,k,j) = r1(2);
         
         
@@ -115,7 +116,7 @@ for j = 1:numel(kL_factor_s)
         MC_matrix_2 = [1*exp((-1i*(phi0+d_phi))-loss), 0; 0, 1*exp(-1i*(phi0)-loss_2)];
         r2 = Tc_matrix_c*MC_matrix_2*Tc_matrix_s*[E_i_laser, 0]';
     
-        result_vector_high(1,k,j) = r2(1);
+        result_vector_low(1,k,j) = r2(1);
         result_vector_high(2,k,j) = r2(2);
 
         ER_kL1(j, k) = 10*log10((abs(result_vector_high(1,k,j))^2)/((abs(result_vector_low(1,k,j))^2)));
@@ -125,6 +126,9 @@ end
 
 %% kL - Loss PLOTS    
    
+    yBox = [10, 10, 40, 40, 10];
+    xBox = [0, alpha(end)*cm, alpha(end)*cm, 0, 0];
+
     figure(Name="ER/loss - kL");
     tiledlayout(1,2)
 
@@ -132,14 +136,18 @@ end
     %plot ER1
     nexttile
     yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+    yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")
     hold on
+    p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+    p.Annotation.LegendInformation.IconDisplayStyle = 'off';
     for j = (1:numel(kL_factor_s))
-        plot(alpha, ER_kL1(j,:), DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha.*cm, ER_kL1(j,:), DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
+
     hold off
     legend
     grid on
-    xlabel("loss [db/m]");
+    xlabel("loss [db/cm]");
     ylabel("Extinction rate [db] port 1")
     title("Extinction rate port 1")
    
@@ -147,15 +155,18 @@ end
     %plot ER2
     nexttile
     yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+    yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")
     hold on
+    p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+    p.Annotation.LegendInformation.IconDisplayStyle = 'off';
     for j = (1:numel(kL_factor_s))
-        plot(alpha, ER_kL2(j,:), DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha.*cm, ER_kL2(j,:), DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
     hold off
     legend
     grid on
-    xlabel("loss [db/m]");
-    ylabel("Extinction rate [db] port 2")
+    xlabel("loss [dB/cm]");
+    ylabel("Extinction rate [dB] port 2")
     title("Extinction rate port 2")
 
 %% PLOT output - loss - kL 
@@ -163,54 +174,54 @@ end
     tiledlayout(2,2)
     
     nexttile
-    plot(alpha, abs(result_vector_high(1,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1))
+    plot(alpha, abs(result_vector_high(1,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1)/pi + "pi")
     hold on
     for j = (2:numel(kL_factor_s))
-        plot(alpha, abs(result_vector_high(1,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha, abs(result_vector_high(1,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
     hold off
     grid on
     legend
-    xlabel("Loss [db/m]")
+    xlabel("Loss [dB/cm]")
     ylabel("Intensity output [(V/m)^2]" )
     title("Output high port 1")
     
     nexttile
-    plot(alpha, abs(result_vector_high(2,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1))
+    plot(alpha, abs(result_vector_high(2,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1)/pi + "pi")
     hold on
     for j = (2:numel(kL_factor_s))
-        plot(alpha, abs(result_vector_high(2,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha, abs(result_vector_high(2,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
     hold off
     grid on
     legend
-    xlabel("Loss [db/m]")
+    xlabel("Loss [dB/cm]")
     ylabel("Intensity output [(V/m)^2]" )
     title("Output high port 2")
 
     nexttile
-    plot(alpha, abs(result_vector_low(1,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1))
+    plot(alpha, abs(result_vector_low(1,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1)/pi + "pi")
     hold on
     for j = (2:numel(kL_factor_s))
-        plot(alpha, abs(result_vector_low(1,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha, abs(result_vector_low(1,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
     hold off
     grid on
     legend
-    xlabel("Loss [db/m]")
+    xlabel("Loss [dB/cm]")
     ylabel("Intensity output [(V/m)^2]" )
     title("Output low port 1")
 
     nexttile
-    plot(alpha, abs(result_vector_low(2,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1))
+    plot(alpha, abs(result_vector_low(2,:,1)).^2, DisplayName="kL Factor = " + kL_factor_s(1)/pi + "pi")
     hold on
     for j = (2:numel(kL_factor_s))
-        plot(alpha, abs(result_vector_low(2,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j))
+        plot(alpha, abs(result_vector_low(2,:,j)).^2, DisplayName="kL Factor = " + kL_factor_s(j)/pi + "*pi")
     end
     hold off
     grid on
     legend
-    xlabel("Loss [db/m]")
+    xlabel("Loss [dB/cm]")
     ylabel("Intensity output [(V/m)^2]" )
     title("Output low port 2")
 
@@ -223,12 +234,12 @@ end
 % 0  - the second line has an attenuation constant of 0
 % -1 - the second line has a fixed attenuation constant
 % Default = 0
-same_loss_second_line = 0;
+same_loss_second_line = 0*(dB/cm);
 
 % Fixed attenuation constant for the second line in [dB/m]. 
 % Irrelevant if 'same_loss_second_line' is other than -1.
-% Default = 0 [dB/m]
-fixed_loss_second_line = 0;
+% Default = 0 [dB/cm]
+fixed_loss_second_line = 0*(dB/cm);
 
 % Set to 1, 0 or -1 to set the value of the attenuation constant of the
 % second line. 
@@ -295,7 +306,9 @@ for j = 1:numel(alpha)
 
         MC_matrix_1 = [exp((-1i*(phi0+d_phi))-loss), 0; 0, exp((-1i*(phi0))-loss_2)];
         
-        result_vector_low = Tc_matrix_c*MC_matrix_1*Tc_matrix_s*[E_i_laser, 0]';
+        r = Tc_matrix_c*MC_matrix_1*Tc_matrix_s*[E_i_laser, 0]';
+        result_vector_high(1) = r(1);
+        result_vector_low(2) = r(2);
 
         %RF LOW
         RF_max_input = 0;
@@ -304,8 +317,11 @@ for j = 1:numel(alpha)
 
         MC_matrix_2 = [exp((-1i*(phi0+d_phi))-loss), 0; 0, exp((-1i*phi0)-loss_2)];
         
-        result_vector_high = Tc_matrix_c*MC_matrix_2*Tc_matrix_s*[E_i_laser, 0]';
-    
+        r = Tc_matrix_c*MC_matrix_2*Tc_matrix_s*[E_i_laser, 0]';
+        result_vector_low(1) = r(1);
+        result_vector_high(2) = r(2);
+
+
         ER_a1(j, k) = 10*log10((abs(result_vector_high(1))^2)/((abs(result_vector_low(1))^2)));
         ER_a2(j, k) = 10*log10((abs(result_vector_high(2))^2)/((abs(result_vector_low(2))^2)));
     end
@@ -315,14 +331,19 @@ end
 %% ER/kL Factor - Loss PLOTS
 figure(Name="ER/kL_factor - loss");
 tiledlayout(1,2)
+    
 
+    xBox = [kL_factor(1),  kL_factor(end), kL_factor(end), kL_factor(1), kL_factor(1)];
     %plot ER1
     nexttile
     yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+    yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")
     xline(pi/4, ":r", "label","pi/4", "HandleVisibility","off")
     hold on
+    p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+    p.Annotation.LegendInformation.IconDisplayStyle = 'off';
     for j=1:numel(alpha)
-        plot(kL_factor, ER_a1(j,:), DisplayName="alpha = " + alpha(j) + " [dB/m]")
+        plot(kL_factor, ER_a1(j,:), DisplayName="alpha = " + alpha(j)*cm + " [dB/cm]")
     end
     hold off
     legend
@@ -335,10 +356,13 @@ tiledlayout(1,2)
     %plot ER2
     nexttile
     yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+    yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")    
     xline(pi/4, ":r", "label","pi/4", "HandleVisibility","off")
     hold on
+    p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+    p.Annotation.LegendInformation.IconDisplayStyle = 'off';
     for j=1:numel(alpha)
-        plot(kL_factor, ER_a2(j,:), DisplayName="alpha = " + alpha(j) + " [dB/m]")
+        plot(kL_factor, ER_a2(j,:), DisplayName="alpha = " + alpha(j)*cm + " [dB/cm]")
     end
     hold off
     grid on
@@ -352,12 +376,12 @@ tiledlayout(1,2)
 
 
 
-kL_factor = [pi/4, pi/4;
-             pi/3, pi/3;
+kL_factor = [pi/4, pi/4*1.10;
+             pi/3, pi/3*1.10;
              ];
-alpha = [0, 0;
-         1, 1;
-         5, 5];
+alpha = [0*(dB/cm), 0*(dB/cm);
+         1*(dB/cm), 1*(dB/cm);
+         5*(dB/cm), 5*(dB/cm)];
 
 
 %% EXTINTION RATE SIMULATION : varying RF input, fixed alpha/coupling factor 
@@ -391,15 +415,20 @@ for j = (1: numel(alpha(:, 1)))
 
             MC_matrix_1 = [exp((-1i*(phi0+d_phi))-loss_1), 0; 0, exp((-1i*(phi0))-loss_2)];
         
-            result_vector_low = Tc_matrix_c*MC_matrix_1*Tc_matrix_s*[E_i_laser, 0]';
-
+            r = Tc_matrix_c*MC_matrix_1*Tc_matrix_s*[E_i_laser, 0]';
+            result_vector_high(1) = r(1);
+            result_vector_low(2) = r(2);
+          
             %RF LOW
 
             d_phi = 0;
             MC_matrix_2 = [exp((-1i*(phi0+d_phi))-loss_1), 0; 0, exp((-1i*phi0)-loss_2)];
         
-            result_vector_high = Tc_matrix_c*MC_matrix_2*Tc_matrix_s*[E_i_laser, 0]';
-    
+            r = Tc_matrix_c*MC_matrix_2*Tc_matrix_s*[E_i_laser, 0]';
+            result_vector_low(1) = r(1);
+            result_vector_high(2) = r(2);
+
+
             ER_rf1(j, k, i) = 10*log10((abs(result_vector_high(1))^2)/((abs(result_vector_low(1))^2)));
             ER_rf2(j, k, i) = 10*log10((abs(result_vector_high(2))^2)/((abs(result_vector_low(2))^2)));
           
@@ -409,16 +438,21 @@ end
 
 %% PLOTS
 
+xBox = [RF(1),  RF(end), RF(end), RF(1), RF(1)];
+
 figure(Name="ER / RF")
 tiledlayout(2,1)
 nexttile
 yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")
 xline(RF_pi, ":r", "label", "RF_p_i", "HandleVisibility","off")
 hold on
+p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+p.Annotation.LegendInformation.IconDisplayStyle = 'off';
 for j = (1: numel(alpha(:, 1)))
     for k = (1: numel(kL_factor(:, 1)))
         data = squeeze(ER_rf1(j,k,:));
-        plot(RF, data, DisplayName="alpha = " + alpha(j,1) + "," +alpha(j,2) + " kL = " + kL_factor(k,1) + "," +kL_factor(k,2)) 
+        plot(RF, data, DisplayName="alpha= " + alpha(j,1)*cm + " [dB/cm]," +alpha(j,2)*cm + "[dB/cm],  kL= " + kL_factor(k,1)/pi + "*pi" + "," +kL_factor(k,2)/pi +"*pi") 
     end
 end
 hold off
@@ -430,12 +464,15 @@ title("ER/RF port 1")
 
 nexttile
 yline(10, ":r", "label","ER=10dB", "HandleVisibility","off")
+yline(40, ":r", "label","ER=40dB", "HandleVisibility","off")
 xline(RF_pi, ":r", "label", "RF_p_i", "HandleVisibility","off")
 hold on
+p = patch(xBox, yBox, "black", "FaceColor", "green", "FaceAlpha", 0.1);
+p.Annotation.LegendInformation.IconDisplayStyle = 'off';
 for j = (1: numel(alpha(:, 1)))
     for k = (1: numel(kL_factor(:, 1)))
         data = squeeze(ER_rf2(j,k,:));
-        plot(RF, data, DisplayName="alpha = " + alpha(j,1) + "," +alpha(j,2) + " kL = " + kL_factor(k,1) + "," +kL_factor(k,2)) 
+        plot(RF, data, DisplayName="alpha= " + alpha(j,1)*cm + " [dB/cm]," +alpha(j,2)*cm + " [dB/cm],  kL= " + kL_factor(k,1)/pi + "*pi" + "," +kL_factor(k,2)/pi + "*pi") 
     end
 end
 hold off
